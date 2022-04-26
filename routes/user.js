42c0b5e05004fe24620f5ddce999c442cf9f2bd9
“rcesonis” <rimvydas.cesonis@gmail.com>
2022-04-26 21:24:00 +0200

    create route for deleting user

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,10 @@ const { Router } = require("express");
 const req = require("express/lib/request");
 const User = require("../models/User");
 const router = new Router();
-const { verifyTokenAndAuthorization } = require("./verifyToken");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
 // Editing user route
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
@@ -20,6 +23,17 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
     );
     res.status(200).json(updateUser);
   } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+// Delete user route
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User deleted");
+  } catch {
     res.status(500).json(e);
   }
 });
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -23,4 +23,19 @@ const verifyTokenAndAuthorization = (req, res, next) => {
     }
   });
 };
-module.exports = { verifyToken, verifyTokenAndAuthorization };
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("No permission for this action");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
