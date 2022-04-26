ecdaeb9cb4e92cd00c4e65ab369b6236a610d8bf
“rcesonis” <rimvydas.cesonis@gmail.com>
2022-04-27 00:55:27 +0200

    Created delete product route

--- routes/product.js
+++ routes/product.js
@@ -20,4 +20,30 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+// Editing product route
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedProduct);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+// Delete product route
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
